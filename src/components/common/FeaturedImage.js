3a83b0496718a2b458bc4b6d7f504225eb743fbb
mohamedsahin <mohamedsahinva@gmail.com>
2024-04-15 07:06:28 +0400

    -

--- src/components/common/FeaturedImage.js
+++ src/components/common/FeaturedImage.js
@@ -1,38 +1,40 @@
-import Image from "next/image";
-import { useState } from "react";
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
+// FeaturedImage.js
 
-export default function FeaturedImage({ src, alt, title, id, width, height }) {
+import { ImageLoadingContext } from '@/src/utils/ImageLoadingContext';
+import React, { useContext, useEffect } from 'react';
 
-  
+const FeaturedImage = ({ src, alt, title, id, width, height }) => {
+  const { loadedImages, markImageAsLoaded } = useContext(ImageLoadingContext);
   const imageUrl = src;
 
-  const [imageLoaded, setImageLoaded] = useState(false);
-  const handleImageLoad = () => {
-    setImageLoaded(true);
-  };
-
-  
-  
+  useEffect(() => {
+    if (!loadedImages[imageUrl]) {
+      // Preload image and mark it as loaded
+      const img = new Image();
+      img.src = imageUrl === null ? "/assets/img/car-placeholder.png" : imageUrl;
+      img.onload = () => {
+        markImageAsLoaded(imageUrl);
+      };
+      img.onerror = () => {
+        // In case of an error loading the image, mark it as loaded to avoid infinite loading
+        markImageAsLoaded(imageUrl);
+      };
+    }
+  }, [imageUrl, loadedImages, markImageAsLoaded]);
 
   return (
     <>
-      {/* {!imageLoaded && 
-        <Skeleton height={"100%"} width={"100%"} />} */}
-
-        <Image
-          // className={imageLoaded ? "" : "opacity-0"}
-          src={imageUrl === null ? "/assets/img/car-placeholder.png" : imageUrl}
-          width={width}
-          height={height}
-          // key={id}
-          loading="lazy"
-          title={title}
-          alt={alt}
-          // onLoad={(image) => {handleImageLoad(image)}} // Pass as an anonymous arrow function
-
-        />
+      {/* Render image based on loadedImages context */}
+      <img
+        src={imageUrl === null ? "/assets/img/car-placeholder.png" : imageUrl}
+        width={width}
+        height={height}
+        title={title}
+        alt={alt}
+        style={{ display: loadedImages[imageUrl] ? "block" : "none" }}
+      />
     </>
   );
-}
+};
+
+export default FeaturedImage;
