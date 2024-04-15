46a810b76ae0392e22f8af779e700926aea042fe
mohamedsahin <mohamedsahinva@gmail.com>
2024-04-15 10:28:07 +0400

    -

--- src/components/common/FeaturedImage.js
+++ src/components/common/FeaturedImage.js
@@ -1,40 +1,38 @@
-// FeaturedImage.js
+import Image from "next/image";
+import { useState } from "react";
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
-import { ImageLoadingContext } from '@/src/utils/ImageLoadingContext';
-import React, { useContext, useEffect } from 'react';
+export default function FeaturedImage({ src, alt, title, id, width, height }) {
 
-const FeaturedImage = ({ src, alt, title, id, width, height }) => {
-  const { loadedImages, markImageAsLoaded } = useContext(ImageLoadingContext);
+  
   const imageUrl = src;
 
-  useEffect(() => {
-    if (!loadedImages[imageUrl]) {
-      // Preload image and mark it as loaded
-      const img = new Image();
-      img.src = imageUrl === null ? "/assets/img/car-placeholder.png" : imageUrl;
-      img.onload = () => {
-        markImageAsLoaded(imageUrl);
-      };
-      img.onerror = () => {
-        // In case of an error loading the image, mark it as loaded to avoid infinite loading
-        markImageAsLoaded(imageUrl);
-      };
-    }
-  }, [imageUrl, loadedImages, markImageAsLoaded]);
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
+  
+  
 
   return (
     <>
-      {/* Render image based on loadedImages context */}
-      <img
-        src={imageUrl === null ? "/assets/img/car-placeholder.png" : imageUrl}
-        width={width}
-        height={height}
-        title={title}
-        alt={alt}
-        style={{ display: loadedImages[imageUrl] ? "block" : "none" }}
-      />
+      {/* {!imageLoaded && 
+        <Skeleton height={"100%"} width={"100%"} />} */}
+
+        <Image
+          // className={imageLoaded ? "" : "opacity-0"}
+          src={imageUrl === null ? "/assets/img/car-placeholder.png" : imageUrl}
+          width={width}
+          height={height}
+          // key={id}
+          loading="lazy"
+          title={title}
+          alt={alt}
+          // onLoad={(image) => {handleImageLoad(image)}} // Pass as an anonymous arrow function
+
+        />
     </>
   );
-};
-
-export default FeaturedImage;
+}
