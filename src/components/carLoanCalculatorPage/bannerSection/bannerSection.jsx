06cc36084a2a2a122b48aed6865b2a2c12eab0ec
mohamedsahin <mohamedsahinva@gmail.com>
2024-09-11 08:35:24 +0400

    -

--- src/components/carLoanCalculatorPage/bannerSection/bannerSection.jsx
+++ src/components/carLoanCalculatorPage/bannerSection/bannerSection.jsx
@@ -1,21 +1,22 @@
 import { carLoanPage } from "@/src/mocks/labels";
 import Image from "next/image";
 
-export default function BannerSection({ setIsOpen }) {
+export default function BannerSection({ setShowModal }) {
   return (
-    <div className="tw-bg-gradient-to-tl tw-from-blue-800 tw-via-blue-600 tw-to-blue-700 tw-my-4  tw-mt-8 tw-rounded-2xl tw-p-10 tw-text-white tw-flex tw-relative tw-overflow-hidden tw-custom-gradient">
+    <div className="tw-bg-gradient-to-tl tw-from-blue-800 tw-via-blue-600 tw-to-blue-700  tw-rounded-2xl md:tw-p-10 tw-p-8 tw-text-white tw-flex tw-relative tw-overflow-hidden tw-custom-gradient">
       <div>
-        <div className="tw-text-4xl sm:tw-w-1/2 tw-leading-snug">
-          {carLoanPage.bannerImageSection.heading}
+        <div className="tw-text-4xl sm:tw-w-1/2 ">
+          Choose the car to calculate loan EMI
         </div>
-        <div className="tw-font-extralight tw-mb-6 tw-w-2/3 tw-opacity-80">
-          {carLoanPage.bannerImageSection.para}
+        <div className=" tw-mb-6 md:tw-w-2/3 tw-opacity-80 tw-mt-3">
+          Get a quick breakdown of your monthly payments and make an informed
+          decision on your next ride.
         </div>
         <button
-          onClick={() => setIsOpen(true)}
-          className="tw-border-solid tw-border-2 tw-rounded-3xl tw-p-2 tw-px-6 tw-my-4"
+          onClick={() => setShowModal(true)}
+          className="tw-border-solid tw-border  tw-font-bold tw-bg-white tw-rounded-3xl tw-p-2 tw-px-6 tw-my-4"
         >
-          {carLoanPage.bannerImageSection.button}
+          Choose Car Now
         </button>
       </div>
       <Image
