0be704b05a5bbab6687d82e25d56f5ec40ea0c7a
mohamedsahin <mohamedsahinva@gmail.com>
2024-10-11 08:48:24 +0400

    -

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -18,6 +18,8 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Provider } from "react-redux";
 import store from "../store";
 import { useAmp } from "next/amp"; // AMP hook for detection
+import Script from 'next/script';
+
 
 function MyApp({ Component, pageProps }) {
   const isAmp = useAmp(); // Detect AMP mode
@@ -47,13 +49,11 @@ function MyApp({ Component, pageProps }) {
         /^\d{4}$/.test(segment)
       );
 
-      // Parse the yearSegment to an integer
       const year = yearSegment ? parseInt(yearSegment, 10) : null;
 
       if (slug) {
         const latestYear = await fetchLatestYear(slug);
 
-        // Update the year only if it's less than 2024
         if (latestYear && year && year < 2024) {
           pathSegments[pathSegments.indexOf(yearSegment)] =
             latestYear.toString();
@@ -68,20 +68,25 @@ function MyApp({ Component, pageProps }) {
     setCanonical();
   }, [router.asPath]);
 
-  // Check if the locale is set to Arabic
-  const isArabicLocale = locale === "ar";
-
   return (
     <>
       <Head>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/assets/img/favicon.ico" type="image/gif" sizes="20x20" />
+
+        {/* Preload the Gilroy Font */}
         <link
-          rel="icon"
-          href="/assets/img/favicon.ico"
-          type="image/gif"
-          sizes="20x20"
+          rel="preload"
+          href="https://cdn.rawgit.com/mfd/09b70eb47474836f25a21660282ce0fd/raw/e06a670afcb2b861ed2ac4a1ef752d062ef6b46b/Gilroy.css"
+          as="style"
+        />
+
+        {/* Load the Gilroy Font */}
+        <link
+          rel="stylesheet"
+          href="https://cdn.rawgit.com/mfd/09b70eb47474836f25a21660282ce0fd/raw/e06a670afcb2b861ed2ac4a1ef752d062ef6b46b/Gilroy.css"
         />
 
         {/* Canonical URL */}
@@ -94,61 +99,6 @@ function MyApp({ Component, pageProps }) {
           />
         )}
 
-        {/* Scripts and tags that should not appear on AMP pages */}
-        {!isAmp && (
-          <>
-            {/* Google Tag Manager */}
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-W564HNC');`,
-              }}
-            />
-
-            {/* AdSense */}
-            <script
-              async
-              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4857144107996534"
-              crossOrigin="anonymous"
-            ></script>
-
-            {/* Custom Font */}
-            <link
-              rel="stylesheet"
-              href="https://cdn.rawgit.com/mfd/09b70eb47474836f25a21660282ce0fd/raw/e06a670afcb2b861ed2ac4a1ef752d062ef6b46b/Gilroy.css"
-              crossOrigin="anonymous"
-            />
-
-            {/* Facebook Pixel */}
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `!function(f,b,e,v,n,t,s)
-                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                  n.queue=[];t=b.createElement(e);t.async=!0;
-                  t.src=v;s=b.getElementsByTagName(e)[0];
-                  s.parentNode.insertBefore(t,s)}(window, document,'script',
-                  'https://connect.facebook.net/en_US/fbevents.js');
-                  fbq('init', '1194042761803181');
-                  fbq('track', 'PageView');`,
-              }}
-            />
-
-            <noscript>
-              <img
-                height="1"
-                width="1"
-                style={{ display: "none" }}
-                src="https://www.facebook.com/tr?id=1194042761803181&ev=PageView&noscript=1"
-              />
-            </noscript>
-          </>
-        )}
-
         {/* Common styles and assets for AMP and non-AMP */}
         <link
           rel="stylesheet"
@@ -156,10 +106,65 @@ function MyApp({ Component, pageProps }) {
         />
       </Head>
 
-      {/* Content */}
       <Provider store={store}>
         <Component {...pageProps} />
       </Provider>
+
+      {/* Lazy load non-critical scripts after page load */}
+      {!isAmp && (
+        <>
+          {/* Google Tag Manager */}
+          <Script
+            id="gtm"
+            strategy="lazyOnload"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-W564HNC');`,
+            }}
+          />
+
+          {/* Facebook Pixel */}
+          <Script
+            id="facebook-pixel"
+            strategy="lazyOnload"
+            dangerouslySetInnerHTML={{
+              __html: `!function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1194042761803181');
+              fbq('track', 'PageView');`,
+            }}
+          />
+
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src="https://www.facebook.com/tr?id=1194042761803181&ev=PageView&noscript=1"
+            />
+          </noscript>
+
+          {/* AdSense */}
+          <Script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4857144107996534"
+            crossOrigin="anonymous"
+          />
+
+          {/* Analytics and Speed Insights */}
+          <Analytics />
+          <SpeedInsights />
+        </>
+      )}
 
       {/* Conditionally render GTM for non-AMP */}
       {!isAmp && (
@@ -171,14 +176,6 @@ function MyApp({ Component, pageProps }) {
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
-      )}
-
-      {/* Analytics and Speed Insights */}
-      {!isAmp && (
-        <>
-          <Analytics />
-          <SpeedInsights />
-        </>
       )}
     </>
   );
