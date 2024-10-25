6669da70235c29946f0fdbc14706a05b32e4aae2
mohamedsahin <mohamedsahinva@gmail.com>
2024-10-25 17:36:05 +0400

    -

--- src/components/car-list/CarCard.js
+++ src/components/car-list/CarCard.js
@@ -1,161 +1,105 @@
-import React from "react";
-import { Card, CardContent, Typography, div } from "@mui/material";
-import OptimizedImage from "../common/image/OptimisedImage";
-import PrimaryButton from "../buttons/PrimaryButton";
-import Link from "next/link";
+import React from 'react';
+import Skeleton from "@mui/material/Skeleton";
+import Link from 'next/link';
+import Image from 'next/image';
+import OptimizedImage from '../common/image/OptimisedImage';
+import PrimaryButton from '../buttons/PrimaryButton';
 
-const CarCard = ({ car }) => {
-  const CarPriceRange = ({ car }) => {
-    const formatPrice = (price) => {
-      if (price === null) {
-        return "TBC*";
-      }
-      return price?.toLocaleString("en-AE", {
-        minimumFractionDigits: 0,
-        maximumFractionDigits: 2,
-      });
-    };
+const CarPriceRange = React.memo(({ minPrice, maxPrice }) => {
+    const formatPrice = (price) =>
+        price?.toLocaleString("en-AE", { minimumFractionDigits: 0, maximumFractionDigits: 2 }) || "TBC*";
 
-    let priceInfo;
+    const priceInfo = minPrice === maxPrice || !maxPrice
+        ? `AED ${formatPrice(minPrice)}*`
+        : `AED ${formatPrice(minPrice)}* - ${formatPrice(maxPrice)}*`;
 
-    if (car?.minPrice === null && car?.maxPrice === null) {
-      // If both minPrice and maxPrice are undefined, display TBD*
-      priceInfo = "TBC*";
-    } else if (car?.minPrice === car?.maxPrice || car?.maxPrice === undefined) {
-      // If min and max prices are the same or maxPrice is undefined, display only one price
-      priceInfo = `AED ${formatPrice(car?.minPrice)}*`;
-    } else {
-      // Display price range
-      priceInfo = `AED ${formatPrice(car?.minPrice)}* - ${formatPrice(
-        car?.maxPrice
-      )}*`;
-    }
+    return <span className="">{priceInfo}</span>;
+});
 
-    return <span>{priceInfo}</span>;
-  };
-
-  const CarEMIDisplay = ({ car }) => {
-    const tenureInMonths = 60; // Loan tenure in months
-
+// Memoized CarEMIDisplay component
+const CarEMIDisplay = React.memo(({ minPrice }) => {
+    const tenureInMonths = 60;
     const calculateEMI = (principal) => {
-      const annualInterestRate = 0.025; // Annual interest rate (2.5%)
-      const monthlyInterestRate = annualInterestRate / 12; // Monthly interest rate
-      const compoundInterestFactor = Math.pow(
-        1 + monthlyInterestRate,
-        tenureInMonths
-      );
-      const emi =
-        (principal * monthlyInterestRate * compoundInterestFactor) /
-        (compoundInterestFactor - 1);
-      return Math.round(emi);
+        const monthlyInterestRate = 0.025 / 12;
+        const factor = Math.pow(1 + monthlyInterestRate, tenureInMonths);
+        return Math.round((principal * monthlyInterestRate * factor) / (factor - 1));
     };
 
-    // Extract all non-zero prices, calculate EMI for each, and find the minimum EMI
-    const emis = calculateEMI(car?.minPrice);
+    const emi = minPrice ? `AED ${calculateEMI(minPrice).toLocaleString("en-AE", { minimumFractionDigits: 0 })}*` : "Not Available";
 
-    // const minEMI = Math.min(...emis);
+    return <p className="tw-mt-1 md:text-md tw-text-sm tw-font-semibold">{emi}</p>;
+});
 
-    // Format the minimum EMI for display
-    const emiString = emis
-      ? `AED ${emis.toLocaleString("en-AE", {
-          minimumFractionDigits: 0,
-          maximumFractionDigits: 2,
-        })}*`
-      : "Not Available";
+const CarCard = ({ car, loading }) => {
+    const { year, featuredImage, brand, model, engine, power, torque, minPrice, maxPrice } = car || {};
+    return (
+        <article className="tw-flex tw-flex-col tw-py-5 tw-bg-white tw-rounded-2xl tw-border tw-border-solid tw-border-zinc-100 tw-shadow-md tw-cursor-pointer tw-transition-all tw-hover:shadow-lg tw-xl:w-full tw-xl:shadow-lg">
+            <p className="tw-z-10 tw-text-xs tw-justify-center tw-self-start tw-py-1.5 tw-pr-2 tw-pl-3 tw-rounded-tr-full tw-rounded-br-full tw-backdrop-blur-md tw-bg-slate-100 tw-font-semibold">
+                Model: {year}
+            </p>
 
-    return <p className="tw-mt-1 tw-text-lg tw-font-bold">{emiString}</p>;
-  };
+            <Link href={`/brands/${brand?.slug}/${year}/${model?.slug}`}>
+                <header className="tw-flex tw-flex-col tw-w-full tw-text-sm tw-leading-4 tw-rounded-2xl tw-text-neutral-900 tw-px-5">
+                    <div className="tw-relative tw-self-center tw--mt-1.5 tw-w-full tw-aspect-[1.69] tw-max-w-[278px] tw-flex tw-justify-center tw-items-center">
+                        <Image
+                            src={featuredImage?.url || "/assets/img/car-placeholder.png"}
+                            alt={`Image of ${model?.name || "Car"}`}
+                            width={featuredImage?.width}
+                            height={featuredImage?.height}
+                            sizes="(max-width: 600px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                            loading="lazy"
+                        />
+                    </div>
+                </header>
+            </Link>
 
-  return (
-    <>
-      <article className="tw-flex tw-flex-col tw-py-5 tw-bg-white tw-rounded-2xl tw-shadow  ">
-        <p className="tw-z-10 tw-text-xs tw-justify-center tw-self-start tw-py-1.5 tw-pr-2 tw-pl-3 tw-rounded-tr-full tw-rounded-br-full tw-backdrop-blur-md tw-bg-slate-100  tw-font-semibold">
-          {" "}
-          Model: {car?.year}
-        </p>
-        <Link
-          href={`/brands/${car?.brand?.slug}/${car?.year}/${car?.model?.slug}`}
-        >
-          <header className="tw-flex tw-flex-col tw-w-full tw-text-sm tw-leading-4 tw-rounded-2xl tw-text-neutral-900 tw-px-5 ">
-            <div className="tw-relative tw-self-center -tw-mt-1.5 tw-w-full tw-aspect-[1.69] tw-max-w-[278px]">
-              <OptimizedImage
-                src={
-                  car?.featuredImage
-                    ? car?.featuredImage
-                    : "/assets/img/car-placeholder.png"
-                }
-                alt={`Image of ${car.name}`}
-                layout="fill"
-                objectFit="contain"
-                sizes="(max-width: 600px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                width={278}
-                height={169}
-                id={car?.id}
-              />
-            </div>
-          </header>
-        </Link>
-        <Link
-          href={`/brands/${car?.brand?.slug}/${car?.year}/${car?.model?.slug}`}
-        >
-          <section className="car-filter-card tw-flex tw-flex-col tw-justify-center tw-mt-1 tw-w-full tw-px-5 ">
-            <div className="tw-flex tw-flex-col">
-              <div className="tw-flex tw-flex-col">
-                <h3 className="tw-tracking-wider tw-text-blue-600 tw-uppercase tw-font-semibold tw-mb-1">
-                  {car?.brand?.name}
-                </h3>
-                <h4 className="tw-text-gray-600 tw-font-semibold tw-mb-0">
-                  {car?.model?.name}
-                </h4>
-              </div>
-              <p className=" tw-text-neutral-900 tw-font-bold tw-mt-2">
-                <CarPriceRange car={car} />
-              </p>
-            </div>
-          </section>
-        </Link>
-        <section className="tw-px-5">
-          <div className=" car-filter-card-spec tw-flex tw-gap-5 tw-justify-between tw-self-center tw-p-4 tw-w-full tw-rounded-lg tw-bg-slate-100 tw-text-neutral-900 tw-mt-2">
-            <div className="tw-flex tw-flex-col">
-              <p className="spec-head tw-leading-5 tw-uppercase tw-text-gray-500">
-                Engine
-              </p>
-              <p className="spec-value tw-font-semibold tw-text-gray-500">
-                {car?.engine}
-              </p>
-            </div>
-            <div className="tw-flex tw-flex-col tw-whitespace-nowrap">
-              <p className="spec-head tw-leading-5 tw-uppercase tw-text-gray-500">
-                Power
-              </p>
-              <p className="spec-value tw-font-semibold tw-text-gray-500">
-              {car?.power}hp
-              </p>
-            </div>
-            <div className="tw-flex tw-flex-col">
-              <p className="spec-head tw-leading-5 tw-uppercase tw-text-gray-600">
-                Torque
-              </p>
-              <p className="spec-value tw-font-semibold tw-text-gray-500">
-                {car?.torque}Nm
-              </p>
-            </div>
-          </div>
-        </section>
-        <footer className="car-filter-card-emi tw-flex tw-gap-5 tw-justify-between tw-self-center tw-items-center tw-mt-4 tw-w-full tw-px-5 ">
-          <div className="tw-flex tw-flex-col tw-my-auto tw-text-neutral-900">
-            <p className="emi-text">EMI Starting from</p>
+            <Link href={`/brands/${brand?.slug}/${year}/${model?.slug}`}>
+                <section className="tw-car-filter-card tw-flex tw-flex-col tw-justify-center tw-mt-1 tw-w-full tw-px-5">
+                    <div className="tw-flex tw-flex-col">
+                        <h6 className="tw-text-blue-600 tw-uppercase tw-font-semibold tw-mb-1 tw-text-xs">
+                            {brand?.name}
+                        </h6>
+                        <h4 className="tw-text-gray-600 tw-font-semibold tw-mb-0 md:text-lg tw-text-sm">
+                            {model?.name}
+                        </h4>
+                        <h4 className="tw-text-neutral-900 tw-font-bold tw-mt-2">
+                            <CarPriceRange minPrice={minPrice} maxPrice={maxPrice} />
+                        </h4>
+                    </div>
+                </section>
+            </Link>
 
-            <CarEMIDisplay car={car} />
-          </div>
-          <PrimaryButton
-            label="View Details"
-            href={`/brands/${car?.brand?.slug}/${car?.year}/${car?.model?.slug}`}
-          />
-        </footer>
-      </article>
-    </>
-  );
+            <section className="tw-px-5 md:tw-block tw-hidden">
+                <div className="tw-car-filter-card-spec tw-flex tw-gap-5 tw-justify-between tw-self-center tw-p-3 tw-w-full tw-rounded-lg tw-bg-slate-100 tw-text-neutral-900 tw-mt-2">
+                    <div className="tw-flex tw-flex-col">
+                        <p className="tw-text-xs tw-leading-5 tw-uppercase tw-text-gray-500">Engine</p>
+                        <p className="tw-text-sm tw-font-semibold tw-text-gray-500">{engine || "N/A"}</p>
+                    </div>
+                    <div className="tw-flex tw-flex-col">
+                        <p className="tw-text-xs tw-leading-5 tw-uppercase tw-text-gray-500">Power</p>
+                        <p className="tw-text-sm tw-font-semibold tw-text-gray-500">{power ? `${power}hp` : "N/A"}</p>
+                    </div>
+                    <div className="tw-flex tw-flex-col">
+                        <p className="tw-text-xs tw-leading-5 tw-uppercase tw-text-gray-500">Torque</p>
+                        <p className="tw-text-sm tw-font-semibold tw-text-gray-500">{torque ? `${torque}Nm` : "N/A"}</p>
+                    </div>
+                </div>
+            </section>
+
+            <footer className="tw-grid tw-grid-cols-2 tw-mt-3 tw-w-full tw-px-5">
+                <div className="tw-flex tw-flex-col tw-my-auto tw-text-neutral-900">
+                    <p className="tw-text-xs tw-text-gray-500">EMI Starting from</p>
+                    <CarEMIDisplay minPrice={minPrice} />
+                </div>
+                <div className="tw-flex tw-justify-end">
+                    <PrimaryButton
+                        label="View Details"
+                        href={`/brands/${brand?.slug}/${year}/${model?.slug}`}
+                    />
+                </div>
+            </footer>
+        </article>
+    );
 };
 
 export default CarCard;
