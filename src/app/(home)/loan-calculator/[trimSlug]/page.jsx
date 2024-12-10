943d99d624eca16cf56f594c30d1dbca91d6b0ca
moeezs82 <mora03033@gmail.com>
2024-12-10 22:00:31 +0500

    model and trim selection flow change frontend

---
+++ src/app/(home)/loan-calculator/[trimSlug]/page.jsx
@@ -0,0 +1,47 @@
+import LoanCalculatorWrapper from '@/components/loan-calculator/LoanCalculatorWrapper';
+import { fetchMetaData } from '@/lib/fetchMetaData';
+import React from 'react'
+
+export async function generateMetadata() {
+    const slug = "loan-calculator"
+
+    // Fetch dynamic metadata for the privacy policy page
+    const metaData = await fetchMetaData(slug);
+
+    // Return the dynamic metadata
+    return {
+        title: metaData?.title ? metaData.title : "Car Loan Calculator: Easily Calculate Your Car Financing Options - Carprices.ae",
+        description: metaData?.description
+            ? metaData.description
+            : "Calculate car loans effortlessly. Get accurate estimates, explore repayment options, and make informed decisions. Plan confidently with CarPrices UAE.",
+        charset: "UTF-8",
+        alternates: {
+            canonical: `https://carprices.ae/loan-calculator`,
+        },
+        keywords: metaData?.keywords || "contact CarPrices.ae, car inquiries UAE, car suggestions UAE, car feedback UAE, automotive assistance UAE, trusted automotive companion, car journey UAE, car prices UAE, contact us CarPrices.ae, automotive world UAE, connect with CarPrices.ae",
+        robots: {
+            index: true,
+            follow: true,
+        },
+        structuredData: {
+            "@context": "https://schema.org",
+            "@type": "WebPage",
+            name: metaData?.title ? metaData.title : "Car Loan Calculator: Easily Calculate Your Car Financing Options - Carprices.ae",
+            description:
+                metaData?.description
+                    ? metaData.description
+                    : "Calculate car loans effortlessly. Get accurate estimates, explore repayment options, and make informed decisions. Plan confidently with CarPrices UAE.",
+            url: "https://carprices.ae/contact-us",  // Using the same canonical URL here
+        },
+        author: "Carprices.ae Team",
+        icon: "./favicon.ico",
+    };
+}
+
+const page = () => {
+  return (
+    <LoanCalculatorWrapper/>
+  )
+}
+
+export default page
