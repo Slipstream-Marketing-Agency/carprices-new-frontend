c82ddaa0ebe6213996be5eb9b2aa4574ddcdd852
moeezs82 <mora03033@gmail.com>
2025-01-08 12:49:35 +0500

    removed extra h1 from model details page

--- src/components/model-component/PriceComponent/PriceComponent.jsx
+++ src/components/model-component/PriceComponent/PriceComponent.jsx
@@ -6,7 +6,7 @@ import Price from '@/utils/Price'
 const MainPriceComponent = ({ currentmodel, year }) => {
   return (
     <>
-      <h1 className="font-semibold text-3xl mb-5">{currentmodel.brand?.name} {currentmodel.name} {year} Price in UAE</h1>
+      <h2 className="font-semibold text-3xl mb-5">{currentmodel.brand?.name} {currentmodel.name} {year} Price in UAE</h2>
       <p>
         {currentmodel.brand?.name} {currentmodel.name} price in the UAE starts at <Price data={currentmodel.price?.min} /> for the base variant and goes all the way up to <Price data={currentmodel.price?.max} /> for the top-spec variant. Check out the {currentmodel.brand?.name} {currentmodel.name} variant-wise price list and available special promo offers below. Also, get the best price by requesting quotes from authorised {currentmodel.brand?.name} dealerships.
       </p>
