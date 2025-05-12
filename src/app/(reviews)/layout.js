7d7048de327a84e90be022670345de8617cd5cf7
Asaddd123 <asad@slipstream.agency>
2025-05-12 17:27:15 +0500

    Update layout.js in reviews adds code

--- src/app/(reviews)/layout.js
+++ src/app/(reviews)/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
      <head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4096282930416593"
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4857144107996534"
           crossOrigin="anonymous"></script>
         <meta name="google-adsense-account" content="ca-pub-4096282930416593" />
       </head>
